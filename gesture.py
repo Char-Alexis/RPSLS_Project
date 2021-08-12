06bb5c6b94d77defb842197f94fcb3d209c00506
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-12 14:16:33 -0400

    Created gesture.py, match.py, player.py files

---
+++ gesture.py
@@ -0,0 +1,7 @@
+class Gestures:
+    def __init__ (self):
+        pass
+    
+    gestures = ["Rock", "Paper", "Scissors", "Lizards", "Spock"]
+
+    
---
+++ match.py
@@ -0,0 +1,10 @@
+print("Welcome to RPSLS Game!")
+
+print("Rules of game:... Best 2 out of 3 wins")
+
+
+print("Play against another player or computer?")
+
+print("Do you want to start game? Yes or No?")
+
+print("Pick your choice: 1, 2, 3, 4, 5")
