c0fc09f08fb1d28f8e41e54a1257509e15b34a6c
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-13 13:33:13 -0400

    Got human.py file to work the way we want it to

--- human.py
+++ human.py
@@ -1,6 +1,5 @@
 from player import Player
 
-
 class Human(Player):
     def __init__(self):
         super().__init__()
@@ -11,8 +10,12 @@ class Human(Player):
         for player in self.gesture_list:
             print(f'Type {index} for {player}')
             index += 1
-        self.picked_gesture = input()
-        print(self.picked_gesture)
+        self.picked_gesture = int(input())
+        # if self.picked_gesture == 0:
+            # for player in self.gesture_list:
+            #     if 
+            # print('You have chosen Spock')
+        print(f'You have chosen {self.gesture_list[self.picked_gesture]}')
 
     def pick_gesture(self):
         pass
@@ -20,4 +23,4 @@ class Human(Player):
 
 
 battle = Human()
-battle.choose_gestures
+battle.choose_gestures()
